2d6e8e1aa217950aa5f7562a7b3c4a3c7e57e97d
SHU CHEN <chenshu8805@gmail.com>
2016-05-02 18:46:45 -0400

    extensible app

---
+++ roomCluster/room/Extensible.rb
@@ -0,0 +1,14 @@
+require_relative 'RoomCluster.rb'
+class Extensible
+
+	def initialize(comparable1, comparable2, numSofa, numFoot, numLove, numStorage)
+		@initialRoom = RoomCluster.new(comparable1, numSofa, numFoot, numLove, numStorage)
+		averageResults(comparable1, comparable2)
+	end
+
+	def averageResults(comparable1, comparable2)
+		@initialRoom.buildUnknownRoom(comparable1, comparable2)
+	end
+end
+
+# Extensible.new(ARGV[0], ARGV[1], ARGV[2], ARGV[3], ARGV[4], ARGV[5])
